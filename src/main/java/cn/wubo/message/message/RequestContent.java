32e5bdbad47f57e1c868767e1c289cfe9ee49b9e
吴博 <wubo_aaa@163.com>
2023-09-18 10:54:48 +0800

    消息中间件

--- src/main/java/cn/wubo/message/message/RequestContent.java
+++ src/main/java/cn/wubo/message/message/RequestContent.java
@@ -31,37 +31,67 @@ public abstract class RequestContent<T> {
     }
 
     public T addDingtalkCustomRobot(String key, Object value) {
-        this.params.getDingtalkCustomRobot().put(key, value);
+        params.getDingtalkCustomRobot().put(key, value);
         return (T) this;
     }
 
     public T addDingtalkMessage(String key, Object value) {
-        this.params.getDingtalkMessage().put(key, value);
+        params.getDingtalkMessage().put(key, value);
         return (T) this;
     }
 
     public T addFeishuCustomRobot(String key, Object value) {
-        this.params.getFeishuCustomRobot().put(key, value);
+        params.getFeishuCustomRobot().put(key, value);
         return (T) this;
     }
 
     public T addFeishuMessage(String key, Object value) {
-        this.params.getFeishuMessage().put(key, value);
+        params.getFeishuMessage().put(key, value);
         return (T) this;
     }
 
     public T addWeixinMessage(String key, Object value) {
-        this.params.getWeixinMessage().put(key, value);
+        params.getWeixinMessage().put(key, value);
         return (T) this;
     }
 
     public T addMailSmtp(String key, Object value) {
-        this.params.getMailSmtp().put(key, value);
+        params.getMailSmtp().put(key, value);
         return (T) this;
     }
 
     public ContentParams getContentParams(String alias) {
         if (aliasParams.containsKey(alias)) return aliasParams.get("alias");
         return params;
+    }
+
+    public T addDingtalkCustomRobot(String alias, String key, Object value) {
+        aliasParams.get(alias).getDingtalkCustomRobot().put(key, value);
+        return (T) this;
+    }
+
+    public T addDingtalkMessage(String alias, String key, Object value) {
+        aliasParams.get(alias).getDingtalkMessage().put(key, value);
+        return (T) this;
+    }
+
+    public T addFeishuCustomRobot(String alias, String key, Object value) {
+        aliasParams.get(alias).getFeishuCustomRobot().put(key, value);
+        return (T) this;
+    }
+
+    public T addFeishuMessage(String alias, String key, Object value) {
+        aliasParams.get(alias).getFeishuMessage().put(key, value);
+        return (T) this;
+    }
+
+    public T addWeixinMessage(String alias, String key, Object value) {
+        aliasParams.get(alias).getWeixinMessage().put(key, value);
+        return (T) this;
+    }
+
+    public T addMailSmtp(String alias, String key, Object value) {
+        aliasParams.get(alias).getMailSmtp().put(key, value);
+        return (T) this;
     }
 }
